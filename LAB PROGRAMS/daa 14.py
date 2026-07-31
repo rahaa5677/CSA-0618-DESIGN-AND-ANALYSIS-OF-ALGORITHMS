cac3ef48d69af22b1ee3c4ad37674cd95bd3d9d9
rahaa5677 <grahaadeepu@gmail.com>
2026-07-31 14:10:47 +0530

    Delete LAB PROGRAMS directory

--- LAB PROGRAMS/daa 14.py
+++
@@ -1,9 +0,0 @@
-# Method A: Using the ** operator
-base = 2
-exponent = 3
-result_op = base ** exponent  # 2 * 2 * 2
-print(result_op)  # Output: 8
-
-# Method B: Using the built-in pow() function
-result_pow = pow(2, 3)
-print(result_pow)  # Output: 8
